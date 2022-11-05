2220d01962add4ecb2428338c82992fc42147d7f
Benjamin <bedw@bedw.me>
2022-11-05 15:02:30 -0300

    Se corrigieron cosas ya que la pagina cambió un poco
    ahora se guardan varios archivos

--- delitosReg.R
+++ delitosReg.R
@@ -2,8 +2,11 @@ require(httr)
 library(jsonlite)
 library(dplyr)
 library(sf)
+library(stringr)
 
-delitosReg  <- function(nreg,año,id){
+delitosReg  <- function(nreg,year,id){
+
+  dir.create(file.path('results'), showWarnings = FALSE)
 
   cookies <- c(
     'ci_session' = as.character(id)
@@ -12,31 +15,44 @@ delitosReg  <- function(nreg,año,id){
   data <- list(
     `metodo` = 'filter',
     `forma` = 'filter',
+    `poligono` = '',
     `region` = as.character(nreg),
     `provincia` = '-1',
     `comuna` = '-1',
     `barrio` = '-1',
     `categoria` = '-1',
-    `new_categoria` = '-1',
-    `new_subcategoria` = '-1',
-    `anio` = as.character(año),
+    `delito[]` = '-1',
+    `anio` = as.character(year),
     `mes` = '-1'
   )
 
-  res <- httr::POST(url = 'https://siedt.spd.gov.cl/index.php/main/find_points', httr::set_cookies(.cookies = cookies), body = data)
+  res <- httr::POST(url = 'https://siedt.spd.gov.cl/index.php/app/find_points', httr::set_cookies(.cookies = cookies), body = data, encode = 'form')
 
   if (res$status_code != 200){
     print(paste("Código de estado:",res$status_code))
   }
   else{
-    df <- fromJSON(content(res, 'text'))$resulset
+    df <- fromJSON(content(res, 'text'))
 
     if (df %>% length() == 0){
       print('No hay resultados')
     }
     else{
-      df <- as.data.frame(df) %>% select(-c(categoria,subcategoria)) %>% arrange(fecha,hora) %>% st_as_sf(coords = c("x","y"), crs = 4326)
+      for (data in df %>% names){
+        if (!str_detect(data,'(table|clock)')) {
+          print(data)
 
+          if (c('x','y') %in% (df[[data]] %>% names) %>% prod()){
+            df[[data]] %>%
+              arrange(fecha,hora) %>%
+              st_as_sf(coords = c("x","y"), crs = 4326) %>%
+              write_sf(paste0('results/',data,'_R',nreg,'_',year,'.gpkg'))
+          }
+          else{
+            df[[data]] %>% write.csv(paste0('results/',data,'_R',nreg,'_',year,'.csv'),row.names = FALSE)
+          }
+        }
+      }
     }
   }
  return(df)
--- main.R
+++ main.R
@@ -4,4 +4,4 @@ source('delitosReg.R')
 id <- '123456...' # Remplazar por el id ci_session. leer Readme
 
 ###### Obtenemos objeto geométrico con los delitos
-df <- delitosReg(nreg = 15, año = 2021, id = id)
+df <- delitosReg(nreg = 15, year = 2021, id = id)
